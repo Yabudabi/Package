951210fade0ce212aff16df465daa86c6d751611
Yabudabi <32882262+Yabudabi@users.noreply.github.com>
2019-12-03 16:34:16 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name = 'Package',
+	url = 'https://github.com/Yabudabi/Package',
+	author = 'Yannick Bertschy'
+	author_email = 'byannick@ethz.ch'
+	packages = ['package']
+	install_requires = ['numpy']
+	version = '0.1'
+	license = 'MIT'
+	description = 'Python package'
+)
